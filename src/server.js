a44d97c348555818a20941ad5899cad83a76c398
Adauto Marques <adautomarques937@gmail.com>
2024-07-22 14:10:53 -0300

    instalando o nodemon

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.get('/message/:id/:user', (request, response) => {
+  const {id, user} = request.params
+   response.send(`Hello Word, id da mensagem ${id}, para o usuario ${user}`) 
+})
+
+app.get('/user', (request, response) => {
+  const { page, limit } = request.query;
+  response.send(`Page: ${page}. mostrar: ${limit} `)
+})
+
 const PORT = 3333;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
