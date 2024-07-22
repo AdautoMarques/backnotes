0cab7958dda83cdc03c5ba76b45d1b01341028a7
Adauto Marques <adautomarques937@gmail.com>
2024-07-22 17:45:39 -0300

    ajustando o projeto

--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
 const express = require('express');
+const routes = require('./routes')
 
 const app = express();
+app.use(express.json())
+app.use(routes)
 
-app.get('/message/:id/:user', (request, response) => {
-  const {id, user} = request.params
-   response.send(`Hello Word, id da mensagem ${id}, para o usuario ${user}`) 
-})
-
-app.get('/user', (request, response) => {
-  const { page, limit } = request.query;
-  response.send(`Page: ${page}. mostrar: ${limit} `)
-})
 
 const PORT = 3333;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 })
+
